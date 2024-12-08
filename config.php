c33ee54dde06805f54abd7506a2f16ef274af56a
satya5570 <80114131+satya5570@users.noreply.github.com>
2024-12-08 11:59:58 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$cid = '836270982612-kssanr4kimnqmapnlonu4k3rsesusrm8.apps.googleusercontent.com';
-$csc = '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';
+$cid = 'client id';
+$csc = 'client secret';
 
-$apik = 'AIzaSyDg3RX003cgh8Nk-mbiHGdpxmo0wUNT9ZU';
+$apik = 'api key';
 
 ?>
